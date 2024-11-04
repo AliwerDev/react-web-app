1e0355e6a8cd61f0ccadacc050b2e067a63f5ea0
Alisher Uralov <aliwerdev@gmail.com>
2024-11-04 18:03:58 +0500

    fsadf

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 const Home: React.FC = () => {
   useEffect(() => {
     const backButton = window.Telegram.WebApp.BackButton;
-    backButton.hide(); // Hide back button on the home page
+    backButton.hide();
   }, []);
 
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Home Page sdfasdfsd</h1>
       <p>Welcome! Select an option to proceed:</p>
 
       <nav>
